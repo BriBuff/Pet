a22df51d65fa2ed9fac2892b868b31f46392663f
Bri Buffington <bri.kudos@gmail.com>
2019-02-02 13:24:31 -0700

    Update breed.js

---
+++ models/breed.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  var breed = sequelize.define("Breed", {
+    breed_name: DataTypes.STRING,
+    breed_energy: DataTypes.INTEGER,
+    breed_exercise_req: DataTypes.INTEGER,
+    breed_ease_training: DataTypes.INTEGER,
+    breed_grooming_req: DataTypes.INTEGER,
+    breed_affection_level: DataTypes.INTEGER,
+    breed_desc: DataTypes.TEXT,
+    breed_photo: DataTypes.STRING
+  });
+  return breed;
+};
+
