b485125eeddf33a3afc8f24edeee4fed5d4cb9c4
Jessica Katelyn Blomer <katelynblomer@gmail.com>
2019-02-10 14:32:24 -0700

    added first javascript logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/allbreeds", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.breed.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
