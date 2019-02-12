897b5a6c6f6ab83cb9ed88d76a58f4f94287c088
Jessica Katelyn Blomer <katelynblomer@gmail.com>
2019-02-12 12:33:21 -0700

    quiz logic done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,110 @@
 var db = require("../models");
+var breedObject = {};
+var compareArray = [];
+
+function compareArrays(arr1, arr2) {
+  var arr3 = [];
+  var sum = 0;
+  for (var i = 0; i < arr1.length; i++) {
+    var newNum = Math.abs(arr1[i] - arr2[i]);
+    arr3.push(newNum);
+  }
+  for (var k = 0; k < arr3.length; k++) {
+    sum += arr3[k];
+  }
+  return sum;
+}
 
 module.exports = function(app) {
-  // Get all examples
   app.get("/api/allbreeds", function(req, res) {
     db.Breed.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
+  app.post("/quiz", function(req, res) {
+    var survey = req.body;
+    var sArray = [];
+    for (var s in survey) {
+      //console.log(survey[s]);
+      for (var i = 0; i < survey[s].length; i++) {
+        sArray.push(survey[s][i]);
+      }
+      console.log(sArray);
+    }
+
+    db.Breed.findAll({
+      attributes: [
+        "breed_name",
+        "breed_energy",
+        "breed_exercise_req",
+        "breed_ease_training",
+        "breed_grooming_req",
+        "breed_affection_level"
+      ]
+    }).then(function (dbExamples) {
+      breedObject = dbExamples;
+      for (var br in breedObject) {
+        var breedArray = [
+          breedObject[br].dataValues["breed_energy"],
+          breedObject[br].dataValues["breed_exercise_req"],
+          breedObject[br].dataValues["breed_ease_training"],
+          breedObject[br].dataValues["breed_grooming_req"],
+          breedObject[br].dataValues["breed_affection_level"]
+        ];
+        compareArray.push(breedArray);
+      }
+      var sumArray = [];
+
+      for (var j = 0; j < compareArray.length; j++) {
+        console.log(compareArray[j]);
+        sumArray.push(compareArrays(compareArray[j], sArray));
+      }
+      console.log("sumArray: " + sumArray);
+      var num = Math.min.apply(null, sumArray);
+      console.log(num);
+      for (var t = 0; t < sumArray.length; t++) {
+        if (sumArray[t] === num) {
+          console.log("index: " + t);
+          break;
+        }
+      }
+      console.log(breedObject[t].dataValues["breed_name"]);
+      res.json(breedObject[t].dataValues["breed_name"]);
+    });
+  });
   // Create a new breed
   app.post("/api/newbreed", function(req, res) {
-    var breed = req.body.name;
     var routeName = breed.replace(/\s+/g, "").toLowerCase();
     db.Breed.create({
-      routeName: routeName,
-      breed_name: breed,
-      breed_energy: req.body.energy,
-      breed_exercise_req: req.body.exercise,
-      breed_ease_training: req.body.training,
-      breed_grooming_req: req.body.grooming,
-      breed_affection_level: req.body.affection,
-      breed_descript: req.body.description,
-      breed_photo: req.body.photo,
-      breed_link: req.body.link
-    })
-      .then(function(data) {
+        routeName: routeName,
+        breed_name: breed,
+        breed_energy: req.body.energy,
+        breed_exercise_req: req.body.exercise,
+        breed_ease_training: req.body.training,
+        breed_grooming_req: req.body.grooming,
+        breed_affection_level: req.body.affection,
+        breed_descript: req.body.description,
+        breed_photo: req.body.photo,
+        breed_link: req.body.link
+      })
+      .then(function (data) {
         res.json(data);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
         res.json(err);
       });
   });
-};
+
+  app.get("/api/:breed", function(req, res) {
+    var b = req.params.breed;
+    db.Breed.findOne({
+      where: {
+        breed_name: b
+      }
+    }).then(function (data) {
+      res.json(data);
+    });
+  });
+}
